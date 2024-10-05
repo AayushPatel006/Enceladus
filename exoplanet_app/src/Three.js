15b7d0eb5cc07de37088c1d66af91d968b54be5c
aayushpatel006 <rudaniaayush1@gmail.com>
2024-10-05 11:37:50 +0530

    executed model with .glb extension

--- exoplanet_app/src/Three.js
+++ exoplanet_app/src/Three.js
@@ -1,35 +1,26 @@
-import * as THREE from 'three';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useGLTF } from '@react-three/drei';
 
-import { useEffect, useRef } from "react";
+// Component to load and display the .glb model
+const Model = () => {
+  const { scene } = useGLTF('/models/CubeSat.glb');
+  return <primitive object={scene} scale={1.5} />;
+};
 
-function MyThree() {
-  const refContainer = useRef(null);
-  useEffect(() => {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
-    refContainer.current && refContainer.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
-  }, []);
+// Main component to setup the Three.js scene
+const MyThree = () => {
   return (
-    <div ref={refContainer}></div>
-
+    <div className="canvas-container">
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <Model />
+      <OrbitControls />
+    </Canvas>
+    </div>
   );
-}
+};
 
-export default MyThree
+export default MyThree;
